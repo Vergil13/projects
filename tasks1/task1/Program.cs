002ae6b8996347d1d4f48f4d65f0600eaf327a5f
Vergil <Doshirarh@gmail.com>
2018-02-28 11:05:54 +0300

    Added template for task 1

---
+++ tasks1/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IMPUT A");
+            double a=Double.Parse(Console.ReadLine());
+            while (a <= 0)
+            {
+                Console.WriteLine("Why you so stupied . Choose another numbers");
+                a = Double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Imput b");
+            double b=Int32.Parse(Console.ReadLine());
+            while (b <= 0)
+            {
+                Console.WriteLine("Why you so stupied . Choose another numbers");
+
+                b = Double.Parse(Console.ReadLine());
+            }
+            double s = a*b;
+            Console.WriteLine("Your rectangle" +  (s));
+            Console.ReadKey();
+        }
+    }
+}
