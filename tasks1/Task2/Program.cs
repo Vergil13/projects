c1d10f90002489b1a76633126ba90d09f9fa0355
Vergil <Doshirarh@gmail.com>
2018-03-01 13:22:00 +0300

    new commit

---
+++ tasks1/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String t = "*";
+            Console.WriteLine("Введите число строк");
+            
+
+            int a = Int32.Parse(Console.ReadLine());
+            for ( int i=0; i<a; i++)
+            {
+               
+                t += "*";
+                Console.WriteLine(t);
+                
+               
+            }
+            Console.ReadKey();
+        } 
+    }
+}
