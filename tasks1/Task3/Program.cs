4c6c5b3f13fc2e6728160002bf974c7b2bcc7268
Vergil <Doshirarh@gmail.com>
2018-03-01 13:22:46 +0300

    new commit 3

---
+++ tasks1/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            
+            int summa=0;
+            for (i=0; i<1000;i++)
+            {
+                if (i % 3 == 0|| i%5==0) 
+                {
+                    summa = summa + i;
+                    
+                }
+
+                
+
+
+
+
+
+            }
+            Console.WriteLine(summa);
+            Console.ReadKey();
+        } 
+    }
+}
