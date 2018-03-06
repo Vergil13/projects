f30dea51f583f17f5cdb09961a512f7a83e51892
Vergil <Doshirarh@gmail.com>
2018-03-06 21:39:45 +0300

    Я пытался но чето не вышло. 1 задача и 1 недоработка

---
+++ Task3/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string t = Console.ReadLine();
+            Console.WriteLine(ReturnAverageWords(t));
+            Console.ReadKey();
+        }
+        static public int ReturnAverageWords(string args)
+        {
+            string[] array = args.Split(new[] { ' ', ',', ';', ':', '-', '?',  },StringSplitOptions.RemoveEmptyEntries );
+            int summ = 0;
+            for (int i= 0; i < array.Length; i++)
+            {
+                 summ+=array[i].Length;
+
+            }
+            int average = summ / array.Length;
+            return average;
+
+        }
+
+    }
+}
---
+++ Task3/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string textstring = Console.ReadLine();
+            char[] c = textstring.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+
+            }
+            Console.WriteLine("Введите вторую строку");
+            string textstring2 = Console.ReadLine();
+            char[] a = textstring2.ToCharArray();
+            for (int y=0; y<a.Length;y++)
+            {
+
+            }
+
+        }
+        static public Returnwords(string args)
+        {
+
+
+
+        }
+    }
+}
