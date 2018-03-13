14ff183bf56e7a3c69b84a1d2268d95720990883
Vergil <Doshirarh@gmail.com>
2018-03-14 00:20:00 +0300

    USER

---
+++ Malaschenko.Alexandr4/Task1/User.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace task1
+{
+    partial class Program
+    {
+        public class User
+        {
+            private string name , secondname , Patronymic ;
+            internal int Age;
+            private DateTime Birthday;
+
+            public string GetName
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    string pattern ="[A-Za-z]";
+                    Regex regex = new Regex(pattern);
+                    if (regex.IsMatch(pattern))
+                        {
+                        name = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Вы ввели не правильное имя");
+                    }
+                }
+            }
+            public string Secondname
+            {
+                get
+                {
+                    return secondname;
+                }
+                set
+                {
+                    secondname = value;
+                }
+            }
+            public string GetPatronymic
+            {
+                get
+                {
+                    return Patronymic;
+                }
+            }
+            public int GetAge
+            {
+                get
+
+                {
+                    DateTime dateTime = DateTime.Now;
+                    return dateTime.Year - Birthday.Year;
+                }
+
+                
+            }
+
+            public DateTime GetBirthday
+            {
+
+                get
+                {
+                    return Birthday;
+
+                }
+
+
+                set
+                {
+                    DateTime dateTimeNow = DateTime.Now;
+                    if ((dateTimeNow.Year - Birthday.Year) < 100) 
+                    Birthday = value;
+                
+                    
+                } 
+            }
+
+        }
+    }
+}
