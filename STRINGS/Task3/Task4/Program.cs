51a66730765b3bb0de8f725c488df61530888a24
Vergil <Doshirarh@gmail.com>
2018-03-14 00:17:17 +0300

    нОВАЯ 4 АЯ

---
+++ STRINGS/Task3/Task4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            for (int x=0; x<50; x++)
+            {
+                TimeSpan timeSpan = new TimeSpan();
+                Stopwatch stopwatch = new Stopwatch();
+                string str = "";
+                 N *=2;
+                stopwatch.Start();
+                for (int i = 0; i < N; i++)
+                {
+                    str += "*";
+                }
+                stopwatch.Stop();
+                timeSpan = stopwatch.Elapsed;
+                Console.WriteLine($"Это цифры работы string     N = {N}  " +  timeSpan.TotalMilliseconds);
+                stopwatch.Reset();
+                stopwatch.Start();
+                
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < N; i++)
+                {
+                    sb.Append("*");
+                }
+                stopwatch.Stop();
+                timeSpan = stopwatch.Elapsed;
+                Console.WriteLine($"Это цифры работы stringbuilder N = {N}   " + timeSpan.TotalMilliseconds+"\n");
+            }
+            Console.ReadKey();
+        }      
+    }
+}
---
+++ STRINGS/Task3/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string StringOne = "";
+            string Stringtwo = "";
+            string Result = "";
+            Console.WriteLine("Введи первую строку");
+            StringOne = Console.ReadLine();
+            Console.WriteLine("Введи вторую строку");
+            Stringtwo = Console.ReadLine();
+            foreach (char ch in StringOne)
+                if (!Stringtwo.Contains(ch))
+                    Result += ch;
+                else
+                {
+                    Result += ch;
+                    Result += ch;
+                }
+            Console.WriteLine("Результат = {0}", Result);
+            Console.ReadKey();
+        }
+       
+        
+
+
+
+        }
+}
+
